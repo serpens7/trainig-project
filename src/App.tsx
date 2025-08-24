821d4c9668f9e4ee13ddb7fbed2331d656805194
serpens7 <jar5000@yandex.ru>
2025-08-24 18:40:24 +0300

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import AboutPage from 'pages/AboutPage/AboutPage';
 import MainPage from 'pages/MainPage/MainPage';
 import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
+import { useTheme } from 'theme/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className='app'>
+        <div className={`app' ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to='/'>Главная</Link>
             <Link to='/about'>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
