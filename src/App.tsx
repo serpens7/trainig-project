4fbc0e402ad7a6c6d2549550ff079ede42fa023b
serpens7 <jar5000@yandex.ru>
2025-08-24 17:40:54 +0300

    routing + chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from 'components/Counter';
 import './index.scss';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from 'pages/AboutPage/AboutPage';
+import MainPage from 'pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className='app'>
-            <Counter />
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                 <Route path='/about' element={<AboutPage />} />
+                 <Route path='/main' element={<MainPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,3 @@
+import { lazy } from "react";
+
+const MainPage = lazy(() => import('./MainPage'));
